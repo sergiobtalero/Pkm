610132c36f03dba36bb20e93035ffaf9281ac778
Sergio Bravo <>
2020-11-02 17:57:05 -0500

    Initial commit

---
+++ Pokedex/Modules/Landing/PkmLandingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PkmLandingViewController.swift
+//  Pokedex
+//
+//  Created by Sergio David Bravo Talero on 2/11/20.
+//
+
+import UIKit
+
+class PkmLandingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Welcome Sergio")
+    }
+}
